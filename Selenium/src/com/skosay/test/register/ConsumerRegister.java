166155c6b4818b6b1874656cb8c428f43bdbbb23
Leirum <murielbaluyot17@gmail.com>
2014-11-27 13:05:29 +0800

    Updated Consumer Register

--- Selenium/src/com/skosay/test/register/ConsumerRegister.java
+++ Selenium/src/com/skosay/test/register/ConsumerRegister.java
@@ -4,20 +4,19 @@ package com.skosay.test.register;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
-
 import com.miscellaneous.test.Miscellaneous;
 
 public class ConsumerRegister  {
+	String loginurl = "http://devapp.skosay.com/";
+	String errorurl = "http://devapp.skosay.com/register#errorMsg";
 
 	public Miscellaneous misc1 = new Miscellaneous();
 	
 	@BeforeClass
 	public void setUp(){
-	misc1.driver = new FirefoxDriver();
-	misc1.driver.get("http://devapp.skosay.com/register");
+	misc1.setURL("http://devapp.skosay.com/register");
+	misc1.driver.manage().window().maximize() ;
 	}
 	
 	@AfterClass
@@ -45,35 +44,33 @@ public class ConsumerRegister  {
 		misc1.endSession();
 	}
 	
-	
+	/*
 	@Test(priority = 0)
+	//User fills-out the form with complete and correct details
 	public void testValid()throws Exception{
-		inputRegister("Juan Dela Cruz", "Jdelacruz@gmail.com", "jdelacruz000", "jdelacruz000");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+		inputRegister("Juan Dela Cruz", "lacruz@gmail.com", "jdelacruz000", "jdelacruz000");
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
+		
 	Thread.sleep(5000);
-	 
-	System.out.println(misc1.driver.getCurrentUrl());
 	
-	if (misc1.driver.getCurrentUrl().equals("http://devapp.skosay.com/")) {
+	if (misc1.driver.getCurrentUrl().equals(loginurl)) {
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Passed!");
 	}else{
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
 		} 
-	misc1.driver.close();
+	endTest();
 	}
 	
 	
 	@Test(priority = 1)
+	//User fills-out the form with an email that was already in use
 	public void testApplyUsedEmail()throws Exception{
 		setUp();
 		inputRegister("Juan Dela Cruz", "jdelacruz@gmail.com", "jdelacruz000", "jdelacruz000");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-	Thread.sleep(5000);
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
 	
 	if(misc1.driver.findElement(By.xpath("/html/body/div/form[2]/div/label/i[2]")).isDisplayed()){
-		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Passed!");
+		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName()+ " Passed!");
 	}else{
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
 		}
@@ -81,58 +78,64 @@ public class ConsumerRegister  {
 	}
 	
 	
-	
 	@Test(priority = 2)
+	//User fills-out the form and tried other valid email format
 	public void testApplyOtherValidEmail()throws Exception{
-		setUp();
 		
-		String [] validEmails = {"niceAndsimple@example.com", "very.common@example.com", "a.little.lengthy.but.fine@dept.example.com", 
+		String [] validEmails = {"niceandsimple@example.com", "very.common@example.com", "a.little.lengthy.but.fine@dept.example.com", 
 				"disposable.style.email.with+symbol@example.com", "other.email-with-dash@example.com", "user@localserver.com", "another_acceptable_example@server.com"};
 		
 		for(int i=0;i<validEmails.length;i++){
-			//initialize();
 			inputRegister("Juan Dela Cruz", validEmails[i], "jdelacruz000", "jdelacruz000");
-			if(misc1.driver.findElement(By.xpath("/html/body/div/div[3]/input")).isEnabled()){
+			misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
+			
+			Thread.sleep(5000);
+			
+			if (misc1.driver.getCurrentUrl().equals(loginurl)){
 				System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName()+ ":"+ validEmails[i]+ " Passed!");
 			}else{
 				System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() +":"+ validEmails[i] + " Failed!");
 			}
 			misc1.driver.close();
+			setUp();
 		}	
 		
 	}
 	
-	
-	
+
 	@Test(priority = 3)
+	//User fills-out the form and tried invalid email format
 	public void testApplyInvalidEmails()throws Exception{
-		setUp();
+		
 		String [] invalidEmails = {"Abc.example.com", "A@b@c@example.com","a\"b(c)d,e:f;g<h>i[j\\k]l@example.com", "just\"not\"right@example.com",
 				"this is\"not\\allowed@example.com", "this\\ still\\\"not\\allowed@example.com","john..doe@example.com", "john.doe@example..com"};
 		
 		for(int i=0;i<invalidEmails.length;i++){
 			//initialize();
 			inputRegister("Juan Dela Cruz", invalidEmails[i], "jdelacruz000", "jdelacruz000");
-			if(misc1.driver.findElement(By.xpath("/html/body/div/form[2]/div/label/i[2]")).isDisplayed()){
+			misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
+			if(misc1.driver.findElement(By.xpath("/html/body/div/div[2]/div/center/h4/font")).isDisplayed()){
 				System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName()+ ":"+ invalidEmails[i]+ " Passed!");
 			}else{
 				System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() +":"+ invalidEmails[i] + " Failed!");
 			}
 			misc1.driver.close();
+			setUp();
 		}	
 	}
 	
 	
 	
 	@Test(priority = 4)
+	//User fills-out the form with mismatched password and confirm password
 	public void testApplyMismatchedPassword()throws Exception{
 		setUp();
 		inputRegister("Juan Dela Cruz", "jdelacruz1@gmail.com", "jdelacruz000", "jdelacruz012");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
 		
 	Thread.sleep(5000);
 	
-	if (misc1.driver.getCurrentUrl().equals("http://devapp.skosay.com/register#errorMsg")){
+	if (misc1.driver.getCurrentUrl().equals(errorurl)){
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Passed!");
 	}else{
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
@@ -143,30 +146,30 @@ public class ConsumerRegister  {
 	
 	
 	@Test(priority = 5)
+	//User fills-out the form with password that is less than 7 characters
 	public void testApplyShortPassword()throws Exception{
 		setUp();
 		inputRegister("Juan Dela Cruz", "jdelacruz@gmail.com", "jdela", "jdela");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
 
 	Thread.sleep(5000);
 	
-	if (misc1.driver.getCurrentUrl().equals("http://devapp.skosay.com/register#errorMsg")){
+	if (misc1.driver.getCurrentUrl().equals(errorurl)){
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Passed!");
 	}else{
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
 		}
 	misc1.driver.close();
 	}
-	
+	*/
 	
 	
 	@Test(priority = 6)
+	//User did not enter a value on password field but entered a value on confirm password field
 	public void testApplyNoValueOnPassword()throws Exception{
-		setUp();
+		//setUp();
 		inputRegister("Juan Dela Cruz", "jdelacruz1@gmail.com", "", "jdelacruz000");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-	Thread.sleep(5000);
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
 	
 	if(misc1.driver.findElement(By.xpath("/html/body/div/div[3]/input")).isEnabled()){
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
@@ -177,14 +180,13 @@ public class ConsumerRegister  {
 	}
 	
 	
-	
+	/*
 	@Test(priority = 7)
+	//User enters a value on password field but did not enter a value on confirm password field
 	public void testApplyNoValueOnConfirm()throws Exception{
 		setUp();
 		inputRegister("Juan Dela Cruz", "jdelacruz1@gmail.com", "jdelacruz000", "");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-	Thread.sleep(5000);
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
 	
 	if(misc1.driver.findElement(By.xpath("/html/body/div/div[3]/input")).isEnabled()){
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
@@ -197,12 +199,11 @@ public class ConsumerRegister  {
 	
 	
 	@Test(priority = 8)
+	//User did not enter values on password and confirm password field
 	public void testApplyNoValueOnPasswordAndConfirm()throws Exception{
 		setUp();
 		inputRegister("Juan Dela Cruz", "jdelacruz1@gmail.com", "", "");
-		misc1.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
-	Thread.sleep(5000);
+		misc1.driver.manage().timeouts().implicitlyWait(5, TimeUnit.MINUTES);
 	
 	if(misc1.driver.findElement(By.xpath("/html/body/div/div[3]/input")).isEnabled()){
 		System.out.println(Thread.currentThread().getStackTrace()[1].getMethodName() + " Failed!");
@@ -211,5 +212,5 @@ public class ConsumerRegister  {
 		}
 	misc1.driver.close();
 	}
-	
+	*/
 }
